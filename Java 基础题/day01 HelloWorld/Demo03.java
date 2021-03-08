ad87835b9db516cc99d76f60c6a634acf9569cae
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-08 22:48:04 +0800

    Create Demo03.java

---
+++ Java 基础题/day01 HelloWorld/Demo03.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args) {
+//       3、【编号：103】 请编写程序，将某位学员的个人信息输出到屏幕上。信息包括姓名（字符串常量）、性别（字符常量）、年龄（整型常量）、身高（单位米，小数常量）、婚否（布尔常量）。输出格式如下：
+//           张三
+//            男
+//            18
+//            1.78
+//            false
+        System.out.println("张三");
+        System.out.println("男");
+        System.out.println(18);
+        System.out.println(1.78);
+        System.out.println(false);
+    }
+}
