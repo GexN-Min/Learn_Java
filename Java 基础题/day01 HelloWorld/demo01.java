c21b86cb2fbab58f3e8bee73b5b2ac360379c0da
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-08 22:43:21 +0800

    Update demo01.java

--- Java 基础题/day01 HelloWorld/demo01.java
+++ Java 基础题/day01 HelloWorld/demo01.java
@@ -1,1 +1,9 @@
-
+public class HomeWork {
+  // 1、【编号：101】 请编写程序，将如下语句输出到屏幕上：
+  //  java是一门跨平台的计算机语言
+  //  被称为一次编写，处处运行
+  public static void main(String[] args) {
+      System.out.println("java是一门跨平台的计算机语言\n被称为一次编写，处处运行");
+    }
+  
+}
