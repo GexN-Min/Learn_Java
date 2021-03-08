9c5623b7daf6d0830fc3a560dc8972316ee29c8f
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-08 22:45:51 +0800

    Create Demo02.java

---
+++ Java 基础题/day01 HelloWorld/Demo02.java
@@ -0,0 +1,26 @@
+public class Demo02 {
+    public static void main(String[] args) {
+      
+//       2、【编号：102】 常量，是指在程序运行过程中，其值不可以发生改变的量。请编写java程序，分别将下列常量数据打印到屏幕上。
+//        字符串常量：		用双引号括起来的内容。
+//        字符串常量：      用双引号括起来的内容。"HelloWorld","黑马程序员"
+//        整数常量：       不带小数的数字。666, -88
+//        小数常量：       带小数的数字。13.14, -5.21
+//        字符常量：       用单引号括起来的内容。'A','0','我'
+//        布尔常量：       布尔值，表示真假。只有两个值：true,false
+//        空常量：       一个特殊的值，空值，null（null值可以直接在输出语句打印吗？）
+
+        System.out.println("HelloWorld");
+        System.out.println("黑马程序员");
+        System.out.println(666);
+        System.out.println(-88);
+        System.out.println('A');
+        System.out.println('0');
+        System.out.println('我');
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println("");
+        // 不能打印空值
+        // System.out.println(null);
+    }
+}
