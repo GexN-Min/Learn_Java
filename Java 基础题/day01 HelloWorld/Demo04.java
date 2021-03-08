924102585643165821eccee43bcdfd1e60a71590
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-08 22:49:19 +0800

    Create Demo04.java

---
+++ Java 基础题/day01 HelloWorld/Demo04.java
@@ -0,0 +1,28 @@
+public class Demo {
+    public static void main(String[] args) {
+      
+//       4、【编号：104】 请编写程序，分别定义8种基本数据类型的变量，并打印变量的值。
+      
+        /*
+        byte, short, int, long, float, double, boolean, char
+         */
+
+        byte a = 1;
+        short a1 = 2;
+        int a3 = 3;
+        long a4 = 4L;
+        float a5 = 0.0f;
+        double a6 = 0.0D;
+        boolean isOk = true;
+        char b = 'b';
+
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(isOk);
+        System.out.println(b);
+    }
+}
