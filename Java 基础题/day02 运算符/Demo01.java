bdd679b890214955dbb5e185f7f5efd96ea92e9f
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-08 23:07:01 +0800

    Create Demo01.java

---
+++ Java 基础题/day02 运算符/Demo01.java
@@ -0,0 +1,22 @@
+// 1、【编号：201】 练习字符串的“+”操作，请分别输出下列表达式的运算结果：
+// "it" + "heima"
+
+// "itheima" + 666
+
+// 666 + "itheima"
+
+// "黑马" + 6 + 66
+
+// 1 + 99 + "年黑马"
+
+public class Demo01 {
+    public static void main(String[] args) {
+
+        System.out.println("it" + "heima");
+        System.out.println("itheima" + 666);
+        System.out.println(666 + "itheima");
+        System.out.println("黑马" + 6 + 66);
+        System.out.println(1 + 99 + "年黑马");
+        
+    }
+}
