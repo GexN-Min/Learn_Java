afb69facb92d9569c69a225c40e7c0024bb27c57
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-13 19:24:58 +0800

    Update Demo01.java

--- Java 基础题/day 05 方法/Demo01.java
+++ Java 基础题/day 05 方法/Demo01.java
@@ -1,1 +1,22 @@
+//1、
+// 【编号：502】 请定义一个方法，
+// 该方法可以实现对int类型的数组进行遍历，
+// 在控制台打印所有元素。实现方法后，
+// 请在主方法中调用方法，查看结果。
+// 例如，
+// 数组为{11, 22, 33}，打印格式为：[11, 22, 33]
 
+import java.util.Arrays;
+
+public class Demo01 {
+    public static void main(String[] args) {
+
+        int[] arr = {11, 22, 33};
+
+        traverseArr(arr);
+    }
+
+    private static void traverseArr(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+}
