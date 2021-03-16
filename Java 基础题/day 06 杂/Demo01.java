5bb5f13fe488b910c0eae01d8705df9e6345b931
GexN-Min <70270958+GexN-Min@users.noreply.github.com>
2021-03-16 19:21:09 +0800

    Update Demo01.java

--- Java 基础题/day 06 杂/Demo01.java
+++ Java 基础题/day 06 杂/Demo01.java
@@ -1,1 +1,21 @@
+package com.itheima.home.day06;
 
+// 1、
+// 【编号：319】 有一个输出语句System.out.print("@")，
+// 表示输出一个"@"，并且不换行。
+// 使用这个语句，在控制台打印出一个四行五列的长方形，效果如下：
+//@@@@@
+//@@@@@
+//@@@@@
+//@@@@@
+
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 21; i++) {
+            System.out.print("@");
+            if (i % 5 == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
